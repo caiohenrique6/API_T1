a7ca976b645f4a304783093d77e55ca118e12183
Caio Henrique <caiohenriquepassos090@gmail.com>
2025-03-26 10:01:24 -0300

    Adicionando CORS e gunicorn

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 #ativar source venv/bin/activate
 import sqlite3
 from flask import Flask, request , jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 
 #route --> endpoint
 
